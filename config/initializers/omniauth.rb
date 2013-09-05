5a3ab5e3a614331b8955ca35d4f6b286a4f19021
Christian Benseler <cbenseler@gmail.com>
2013-09-05 09:01:05 -0300

    logout

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == "development" || Rails.env == "test"
   provider :facebook, '163465247039548', 'b398d0ddaec71bf3d139dc793371fd5d'
   else
-  provider :facebook, 'XXXXXXX', 'XXXXXXXXX'
+  provider :facebook, '170911729597020', '3a0629e01e8b176a5f4769c07060e7d1'
   end
  
 end
